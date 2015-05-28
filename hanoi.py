08a75fffdd09555b43bcc2e080637cebd48e493a
Mariane Siqueira Machado <mariane.machado@gmail.com>
2015-05-28 02:53:18 -0300

    Improving hanoi

--- hanoi.py
+++ hanoi.py
@@ -1,29 +1,30 @@
-class Peg():
+class Peg:
 	def __init__(self,values,name):
 		self.v = values
 		self.name = name
 
-p1 = Peg([3,2,1,0], "pegA") #disco 3 eh o de maior raio
-p2 = Peg([], "pegB")
-p3 = Peg([], "pegC")
+pegA = Peg([3,2,1,0], "pegA") #disco 3 eh o de maior raio
+pegB = Peg([], "pegB")
+pegC = Peg([], "pegC")
 
 def hanoi(n, src, dst, tmp):
 	if (n > 0):
-		hanoi(n-1, src, tmp, dst)
-		src.v.remove(n)
-		dst.v.append(n)
+		hanoi(n-1, src, tmp, dst) # move to middle all (n-1)
+		move(n, src, dst)
 		print "move disc %d from %s to %s" %(n, src.name, dst.name)
 		hanoi(n-1, tmp, dst, src)
-	else:
+	else: # n == 0
 		print "move disc %d from %s to %s" %(n, src.name, dst.name)
-		src.v.remove(n)
-		dst.v.append(n)
+		move(n, src, dst)
 
+def move(n, src, dst):
+	src.v.remove(n)
+	dst.v.append(n)
 
-print p1.v
-print p2.v
-print p3.v
-hanoi(3, p1, p3, p2)
-print p1.v
-print p2.v
-print p3.v
+print pegA.v
+print pegB.v
+print pegC.v
+hanoi(3, pegA, pegC, pegB)
+print pegA.v
+print pegB.v
+print pegC.v
